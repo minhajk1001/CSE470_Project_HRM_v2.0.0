dcfb6fb65c485851475a8e2811a40aa08da81ae1
Md. Minhajul Islam <47735234+minhajk1001@users.noreply.github.com>
2022-08-12 11:21:14 -0700

    required added and type

--- resources/views/addEmployee.blade.php
+++ resources/views/addEmployee.blade.php
@@ -11,13 +11,14 @@
             <form method="post" action="submit">
                 @csrf
                 <label>ID:</label>
-                    <input type="number" name="ID">
+                    <input type="number" name="ID" required>
                 <label>Full Name:</label>
-                    <input type="text" name="fullName">
+                    <input type="text" name="fullName" required>
                 <label>Email:</label>
-                    <input type="email" name="email">
-                <label>City Name:</label>
-                <select name="designation">
+                    <input type="email" name="email" required>
+                <label>Designation:</label>
+                <select name="designation" class="select" required>
+                    <option value=""></option>
                     <option value="Senior System Designer">Senior System Designer</option>
                     <option value="Junior Software Engineer">Junior Software Engineer</option>
                     <option value="Java Developer">Java Developer</option>
@@ -29,11 +30,12 @@
                     <option value="Systems Software Engineer">Systems Software Engineer</option>
                 </select>
                 <label>Bank Account No.:</label>
-                    <input type="number" name="bankAccountNo">
+                    <input type="number" name="bankAccountNo" required>
                 <label>Salary:</label>
-                    <input type="text" name="salary"><br>
+                    <input type="text" name="salary" required><br>
                 <label>Salary Status:</label>
-                <select name="salaryStatus">
+                <select name="salaryStatus" class="select" required>
+                    <option value=""></option>
                     <option value="Jan">jan</option>
                     <option value="Feb">Feb</option>
                     <option value="Mar">Mar</option>
@@ -50,5 +52,57 @@
                 <button type="submit"  value ="Add Employee">Submit</button>
             </form>
         </div>
+        <style type="text/css">
+            div {
+                border-radius: 5px;
+                background-color: #f2f2f2;
+                padding: 20px;
+            }
+            label{
+                
+            }
+            h2{
+                text-align: center;
+            }
+
+            form {  
+                align: center;
+                max-width: 280px;
+                margin: auto;
+                padding: 0;    
+            }  
+            button {
+                background-color: #4CAF50;
+                border: none;
+                color: white;
+                padding: 10px 131px;
+                text-align: center;
+                text-decoration: none;
+                display: inline-block;
+                font-size: 16px;
+                margin: 30px 2px;
+                cursor: pointer;
+            }
+            input[type=text]{
+                text-align: center;
+                width: 300px;
+                padding: 5px;
+	        }
+            input[type=number]{
+                text-align: center;
+                width: 300px;
+                padding: 5px;
+	        }
+            input[type=email]{
+                text-align: center;
+                width: 300px;
+                padding: 5px;
+	        }
+            .select{
+                text-align: center;
+                width: 315px;
+                padding: 5px;
+            }
+        </style>
     </body>
 </html>
