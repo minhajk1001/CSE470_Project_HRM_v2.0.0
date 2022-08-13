4d7d90514a1b6678910bd36289ac2bd95b0a1715
Md. Minhajul Islam <47735234+minhajk1001@users.noreply.github.com>
2022-08-13 09:48:01 -0700

    half

--- resources/views/announcement.blade.php
+++ resources/views/announcement.blade.php
@@ -4,6 +4,14 @@
         <title>Announcements</title>
     </head>
     <body>
+    <h2 class="text-center">Pay Roll System</h2>
+        <div class = "container">
+            <form method="post" action="giveSalary">
+                @csrf
+                    <input type="text" name="announcement">
+                <button type="submit"  value ="uploadAnnouncement">Announce</button>
+            </form>
+        </div>
         <h2 class="text-center">
             <?php
             echo "Today " . date("Y/m/d") ;
@@ -28,6 +36,30 @@
         </div>
         
         <style>
+            form {  
+                align: center;
+                max-width: 280px;
+                margin: auto;
+                padding: 0;    
+            }
+            input[type=text]{
+                text-align: center;
+                width: 358px;
+                height: 100px;
+                padding: 5px;
+	        }
+            button {
+                background-color: #4CAF50;
+                border: none;
+                color: white;
+                padding: 10px 148px;
+                text-align: center;
+                text-decoration: none;
+                display: inline-block;
+                font-size: 16px;
+                margin: 10px 2px;
+                cursor: pointer;
+            }
             body{
                 background-color: #f2f2f2;
             }
